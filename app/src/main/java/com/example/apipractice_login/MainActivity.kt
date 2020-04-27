18dacea52d1c5567b69b5a0fcf8f8d4ce121fea9
eunjung04 <peunj04@gmail.com>
2020-04-27 20:16:27 +0900

    로그인버튼->아이디/비번가지고 서버에 로그인 요청->응답 로그로 확인

--- app/src/main/java/com/example/apipractice_login/MainActivity.kt
+++ app/src/main/java/com/example/apipractice_login/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.apipractice_login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.apipractice_login.ServerUtil.ServerUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -20,7 +23,21 @@ class MainActivity : BaseActivity() {
             val inputId = idEdt.text.toString() // cho881020 => 조경진
             val inputPw = pwEdt.text.toString() // asdf1234
 
-//            로그인?
+//            서버로 로그인 요청=>ServerUtil클래스 기능 활용
+            ServerUtil.postRequestLogin(mContext, inputId, inputPw, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+
+                    //실제로 응답을 받은걸 분서석해서=>대응
+
+
+                    //임시로 서버 응답 확인 하기 위한 코드
+                    Log.d("서버응답JSON", json.toString())
+
+                }
+
+            })
+
+
 
         }
 
